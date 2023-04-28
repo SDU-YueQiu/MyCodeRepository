34340443993be823d01093908ef9d1d8d438dd95
纯真丁一郎 <iop18653346184@hotmail.com>
2023-04-29 00:14:13 +0800

    4.28

--- Clions/week10/wq.cpp
+++ Clions/week10/wq.cpp
@@ -1,63 +1,95 @@
-#include <cmath>
 #include <iostream>
 using namespace std;
-template<class H>
-class minheap
-{
-private:
-    H *heapdata;
-    int number;
-    int size;
-    void push(int x,int pos)
-    {
-        heapdata[pos] = x;
-        int i=pos;
-        while(i>1&&heapdata[i]<heapdata[i/2])
-        {
-            swap(heapdata[i],heapdata[i/2]);
-            i/=2;
-        }
-    }
-    void Create()
-    {
-        cout << "元素数目\n";
-        int a;
-        cin >> a;
-        number = a;
-        cout << "请输入元素\n";
-        for (int i = 0; i < a; i++)
-        {
-            H b;
-            cin >> b;
-            push(b,i);
-        }
-    }
 
-public:
-    minheap(int a)
-    {
-        if (a <= 0)
-            return;
-        number = 0;
-        size = a;
-        heapdata = new H[size];
-        Create();
-    }
-    ~minheap()
-    {
-        delete[] heapdata;
-    }
-    void print()
-    {
-        for (int i = 0; i < number; i++)
-            cout << heapdata[i] << " ";
-    }
+struct tNode {
+    int data;
+    tNode *next;
 };
+tNode *head;
+void createLink(int a[], int n);
+int cnt();
+void add(int n);
 
+int cnt()
+{
+    int j = 0;
+    tNode *p = head;
+    while (p) {
+        if (p->data % 2 == 1) {
+            j++;
+        }
+        p = p->next;
+    }
+    return j;
+}
+void add(int n)
+{
+    tNode *p = head;
+    /*label:
+    for (int i = 0; i < 7; i++) {
+        while (p) {
+            if (p->data == i) {
+                i++;
+                goto label;
+            }
+            p = p->next;
+        }*/
+    //这样套两层循环只能处理七个节点，其他数据不一定是七个，而且这样跳出循环也有些不知所云
+    while (p)
+    {
+        if (p->data == n)
+            return;
+        p = p->next;
+    }
+    //运行到这里说明没有n
+    tNode *pre = head;
+    while (pre->next)//下一个节点为null则说明这个节点是尾节点
+        pre = pre->next;
+    tNode *t = new tNode;
+    t->next = NULL;
+    t->data = n;
+    pre->next = t;
+}
+
+void createLink(int a[], int n)
+{
+    tNode *node;
+    head = NULL;
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        node = new tNode;
+        node->data = a[i];
+        node->next = NULL;
+
+        node->next = head;
+        head = node;
+        //这样是一直往前面加节点，有点别扭，习惯上是往后面加节点
+    }
+}
+
+int last()
+{
+    tNode *p = head;
+    int n;
+    while (p)
+    {
+        n = p->data;
+        p = p->next;
+    }
+    return n;
+}
 
 int main()
 {
-    minheap<int> x(INT_MAX);
-    x.print();
+    int a[] = {1, 2, 3, 4, 5};
+
+    createLink(a, 5);
+    cout << cnt() << " ";// 链表中有5个节点，3个奇数
+    int m;
+    cin >> m;
+    add(m);
+    cout << last() << endl;// 最后一个节点的值应该是7
+
     return 0;
 }
---
+++ Clions/week11/9-9.cpp
@@ -0,0 +1,8 @@
+//9_9.cpp
+#include "LinkedList.h"
+
+int main() {
+    Calculator c;
+    c.run();
+    return 0;
+}
